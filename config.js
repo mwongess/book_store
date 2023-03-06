9b597c96bc3e2497cc95a8811d57ada67336be65
mwongess <amosmwongelah@gmail.com>
2023-03-06 22:07:19 +0300

    Implement web services

---
+++ config.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Lethal',
+    database: 'bookStore'
+})
+
+
+connection.connect((error)=>{
+    if(error){
+        console.error('Error connecting to the database:' ,error)
+    }
+    console.log('Connected to the database!')
+})
+
+module.exports = connection
---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const json = express.json
+const connection = require("./config");
+const {v4: uuidv4} = require('uuidv4')
+
+const app = express();
+app.use(json())
+
+const PORT = 4000;
+
+app.get("/books", (req, res) => {
+  try {
+    const query = "SELECT * FROM books;";
+    connection.query(query, (error, results) => {
+        if(error){
+            res.json({ERROR: error})
+            return
+        }
+        res.json({BOOKS: results})
+
+    });
+  } catch (error) {
+    res.json({ERROR: error})
+  }
+});
+
+app.post("/books",(req, res)=>{
+    try {
+        const id = uuidv4
+        const {name} = req.body
+        const query = `INSERT INTO books VALUES ("${id}", "${name}")`
+
+        connection.query(query, (error,info)=>{
+            if(error){
+                res.json({error})
+                return
+            }
+            res.json({info})
+        })
+    } catch (error) {
+        
+    }
+})
+app.listen(PORT, () => {
+  console.log(`Server up and running on port ${PORT}`);
+});
