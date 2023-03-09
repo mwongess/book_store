687ee2cb2742b999bea62ec0b8395761084e5e72
mwongess <amosmwongelah@gmail.com>
2023-03-09 17:44:32 +0300

    Update your user and password in this file

--- config/config.js
+++ config/config.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'Lethal',
+    user: '',
+    password: '',
     database: 'bookStore'
 })
 
