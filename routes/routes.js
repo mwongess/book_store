60257c5cb79a917758b2ed9a06546b0d6548a823
mwongess <amosmwongelah@gmail.com>
2023-03-09 17:36:59 +0300

    Advance Routing

---
+++ routes/routes.js
@@ -0,0 +1,11 @@
+const { getBooks, getBook, addBook, updateBook, deleteBook } = require("../controlllers/controllers");
+const express = require('express');
+const router = express.Router();
+
+router.get("/", getBooks);
+router.get("/:id", getBook);
+router.post("",addBook );
+router.put("/:id", updateBook);
+router.delete("/:id",deleteBook);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const json = express.json;
 const startServer = require("./lib/boot");
-const { getBooks, getBook, addBook, updateBook, deleteBook } = require("./controlllers/controllers");
+const router = require("./routes/routes");
 
 const app = express();
+
+app.use('/books', router)
 app.use(json()); 
-
-
-app.get("/books", getBooks);
-app.get("/books/:id", getBook);
-app.post("/books",addBook );
-app.put("/books/:id", updateBook);
-app.delete("/books/:id",deleteBook);
 
 startServer(app)
